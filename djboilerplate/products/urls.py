d394e3f670270cd34df2273670e3ce547c5f34cd
Marcello Benigno <benigno.marcello@gmail.com>
2018-04-10 18:08:22 -0300

    adicionando funcoes do crud

--- djboilerplate/products/urls.py
+++ djboilerplate/products/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'products'
 
 urlpatterns = [
-    url(r'', views.list_, name='list'),
+    path(r'lista/', views.list_, name='list'),
+    path(r'novo/', views.create, name='create'),
+    path(r'<int:pk>/editar/', views.update, name='update'),
+    path(r'<int:pk>/deletar/', views.delete, name='delete'),
 
 ]
--- djboilerplate/products/views.py
+++ djboilerplate/products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import ProductForm
 from .models import Product
 
 
@@ -9,3 +10,41 @@ def list_(request):
         'product_list': product_list,
     }
     return render(request, 'products/list.html', context)
+
+
+def create(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('products:list')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'products/form.html', context)
+
+
+def update(request, pk):
+    obj = get_object_or_404(Product, pk=pk)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect('products:list')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'products/form.html', context)
+
+
+def delete(request, pk):
+    obj = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('products:list')
+
+    context = {
+        'obj': obj,
+    }
+    return render(request, 'products/delete.html', context)
