99725cb298c84df6c9ea430ac2ed78fdd280174f
Marcello Benigno <benigno.marcello@gmail.com>
2018-04-15 16:27:04 -0300

    fix

--- djboilerplate/urls.py
+++ djboilerplate/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -9,6 +7,3 @@ urlpatterns = [
     path('produtos/', include('djboilerplate.products.urls', namespace='products')),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
